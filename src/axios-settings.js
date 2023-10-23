2fa6b4f3e8396bee88e9de950a5609afb5c9578b
Roma Yezerskyi <yezerskyi.roman@gmail.com>
2023-10-23 22:22:00 +0300

    fix

--- src/axios-settings.js
+++ src/axios-settings.js
@@ -8,7 +8,7 @@ import { setAlert } from './context/alert-context/alert-actions';
 import { useDispatch } from 'react-redux';
 
 const API = axios.create({
-    baseURL: "https://localhost:1488/api/",
+    baseURL: "https://fyb-back.azurewebsites.net/api/",
     withCredentials: true,
 });
 
